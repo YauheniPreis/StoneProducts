fbbf6fa783146aa8dc7fbc87321d8b14cfb0c191
Yauheni Preis <yauheni.preis@yellow.systems>
2024-03-15 06:36:48 +0300

    updated next config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    emotion: true,
-  },
-  images: {
-    domains: ["images.unsplash.com"],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
