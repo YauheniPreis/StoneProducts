2b6256e005afa9b70d196759c9ca96af5fc4a5bf
Yauheni Preis <yauheni.preis@yellow.systems>
2024-05-15 04:02:45 +0300

    added main page content

--- src/constants/common.constants.ts
+++ src/constants/common.constants.ts
@@ -3,8 +3,17 @@ export const PAGES = [
   { title: "Каталог продукции", link: "/products" },
   { title: "Наши работы", link: "/about" },
 ];
+export const BENEFITS = [
+  "Абсолютно невидимые стыки",
+  "Смета не меняется в процессе",
+  "Бесплатный замер",
+  "500 оттенков камня",
+];
+export const MAIN_TITLE =
+  "ИЗГОТОВИМ СТОЛЕШНИЦЫ ИЗ ИСКУССТВЕННОГО КАМНЯ ЗА 5-7 ДНЕЙ. ЗАМЕР И МОНТАЖ БЕСПЛАТНО";
 export const PHONE_NUMBER = "+7 (925) 809-4985";
 export const FEEDBACK_BUTTON_TEXT = "Заказать расчёт";
+export const CALCULATE_COST_BUTTON_TEXT = "Рассчитать стоимость столешницы";
 export const CONTACTS_TITLE = "Контакты";
 export const EMAIL_TEXT = "yauheni.preis@gmail.com";
 export const OPERATING_MODE_TITLE = "Режим работы";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,126 @@
-import * as React from "react";
+import React from "react";
 
-import { Typography } from "@mui/material";
+import { Box, Button, List, ListItem, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+import {
+  BENEFITS,
+  CALCULATE_COST_BUTTON_TEXT,
+  MAIN_TITLE,
+} from "constants/common.constants";
+
+const useStyles = makeStyles((theme) => ({
+  homeContainer: {
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column-reverse",
+    },
+  },
+  imageContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "50%",
+    height: "100%",
+  },
+  contentContainer: {
+    padding: "20px 40px",
+    display: "flex",
+    flexDirection: "column",
+    width: "50%",
+    height: "100%",
+    [theme.breakpoints.down("md")]: {
+      alignItems: "center",
+      width: "100%",
+    },
+  },
+  title: {
+    fontSize: "32px",
+    fontWeight: 700,
+    textTransform: "uppercase",
+    lineHeight: "45px",
+    color: "#000",
+    [theme.breakpoints.down("lg")]: {
+      fontSize: "25px",
+    },
+    [theme.breakpoints.down("md")]: {
+      textAlign: "center",
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "20px",
+    },
+  },
+  list: {
+    padding: "25px 25px",
+  },
+  listItem: {
+    padding: "0",
+    marginBottom: "15px",
+    "&:last-child": {
+      marginBottom: "0",
+    },
+  },
+  listItemText: {
+    fontSize: "17px",
+    fontWeight: 400,
+    color: "#000",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "14px",
+    },
+  },
+  button: {
+    height: "55px",
+    width: "380px",
+    borderRadius: "6px",
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
+  },
+  buttonText: {
+    fontSize: "15px",
+    fontWeight: 600,
+    lineHeight: 1.55,
+    color: "#ffffff",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "13px",
+    },
+  },
+}));
 
 const Home = () => {
-  return <Typography>Main</Typography>;
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.homeContainer}>
+      <Box className={classes.imageContainer}></Box>
+
+      <Box className={classes.contentContainer}>
+        <Typography className={classes.title}>{MAIN_TITLE}</Typography>
+        <List className={classes.list}>
+          {BENEFITS.map((text, index) => (
+            <ListItem key={index} className={classes.listItem}>
+              <Typography className={classes.listItemText}>{text}</Typography>
+            </ListItem>
+          ))}
+        </List>
+        <Button
+          variant="contained"
+          size="large"
+          color="error"
+          className={classes.button}
+        >
+          <Typography className={classes.buttonText}>
+            {CALCULATE_COST_BUTTON_TEXT}
+          </Typography>
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
