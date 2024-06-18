77c3e6e098a15e465f44ce68e3e7d99cfb12a7b9
Yauheni Preis <yauheni.preis@yellow.systems>
2024-06-18 05:20:09 +0300

    added three new default pages

---
+++ src/pages/installation/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Box } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+import ImageSwiper from "components/ImageSwiper/ImageSwiper";
+
+import { ABOUT_IMAGES, ABOUT_TITLE } from "constants/common.constants";
+
+const useStyles = makeStyles(() => ({
+  contentContainer: {
+    margin: "50px 0",
+    width: "100%",
+  },
+}));
+
+const Installation = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.contentContainer}>
+      <ImageSwiper images={ABOUT_IMAGES} title={ABOUT_TITLE} />
+    </Box>
+  );
+};
+
+export default Installation;
