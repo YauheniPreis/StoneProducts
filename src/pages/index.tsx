eefbc05551bee219d1b177e1c143e7fd3feee059
Yauheni Preis <yauheni.preis@yellow.systems>
2024-11-14 05:26:28 +0300

    updated next image import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { useRouter } from "next/router";
 
 import { Box, Tab, Tabs, Typography } from "@mui/material";
